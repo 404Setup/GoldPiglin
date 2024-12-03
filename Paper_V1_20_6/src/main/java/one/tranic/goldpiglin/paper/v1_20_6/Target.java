631b61f5d7d9a5910946ac262bf6f7913f3e0d5d
404Setup <153366651+404Setup@users.noreply.github.com>
2024-12-03 13:12:16 +0800

    Provide nms for versions above 1.20.6

--- Paper_V1_20_6/src/main/java/one/tranic/goldpiglin/paper/v1_20_6/Target.java
+++ Paper_V1_20_6/src/main/java/one/tranic/goldpiglin/paper/v1_20_6/Target.java
@@ -1,9 +1,10 @@
 package one.tranic.goldpiglin.paper.v1_20_6;
 
-import net.minecraft.tags.TagKey;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.behavior.BehaviorUtils;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.armortrim.ArmorTrim;
+import net.minecraft.world.item.armortrim.TrimMaterials;
 import one.tranic.goldpiglin.common.BaseTarget;
 import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.entity.Entity;
@@ -19,7 +20,8 @@ public class Target extends BaseTarget {
     @Override
     public boolean readItemStack(ItemStack itemStack) {
         net.minecraft.world.item.ItemStack item = CraftItemStack.unwrap(itemStack);
-        System.out.println(item.getPrototype().toString());
-        return false;
+        ArmorTrim trim = item.getComponents().get(DataComponents.TRIM);
+        if (trim == null || trim.material() == null) return false;
+        return trim.material().is(TrimMaterials.GOLD);
     }
 }
