76d3b479608e229263a5dfcf6e6b2a76dc4923a5
404Setup <153366651+404Setup@users.noreply.github.com>
2024-12-03 15:27:39 +0800

    fix: tab completion
    - fix: tab completion
    - Better message

--- src/main/java/one/tranic/goldpiglin/command/GPiglinCommand.java
+++ src/main/java/one/tranic/goldpiglin/command/GPiglinCommand.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 public class GPiglinCommand extends Command {
     private final JavaPlugin plugin;
+    private final String permissionMessage = ChatColor.AQUA + "[GoldPiglin] " + ChatColor.RED + "You don't have permission to use this command!";
 
     public GPiglinCommand(JavaPlugin plugin) {
         super("gpiglin");
@@ -20,10 +21,14 @@ public class GPiglinCommand extends Command {
         this.setUsage("/gpiglin <reload|version>");
     }
 
+    private String getUsageMessage() {
+        return ChatColor.AQUA + "[GoldPiglin] " + ChatColor.RED + "Usage: " + this.getUsage();
+    }
+
     @Override
     public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, String[] args) {
         if (args.length == 0) {
-            sender.sendMessage(ChatColor.RED + "Usage: " + this.getUsage());
+            sender.sendMessage(getUsageMessage());
             return true;
         }
         if (args[0].equalsIgnoreCase("reload")) {
@@ -34,41 +39,34 @@ public class GPiglinCommand extends Command {
             executeVersion(sender);
             return true;
         }
-        sender.sendMessage(ChatColor.RED + "Usage: " + this.getUsage());
+        sender.sendMessage(getUsageMessage());
         return true;
     }
 
     private void executeReload(@NotNull CommandSender sender) {
         if (!sender.hasPermission("goldpiglin.command.reload")) {
-            sender.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
+            sender.sendMessage(permissionMessage);
             return;
         }
         Config.reload(this.plugin);
-        sender.sendMessage(ChatColor.GREEN + "Reloaded!");
+        sender.sendMessage(ChatColor.AQUA + "[GoldPiglin] " + ChatColor.GREEN + "The configuration file has been reloaded. Some changes require reloading the plugin or restarting the server to take effect.");
     }
 
     private void executeVersion(@NotNull CommandSender sender) {
         if (!sender.hasPermission("goldpiglin.command.version")) {
-            sender.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
+            sender.sendMessage(permissionMessage);
             return;
         }
-        if (GoldPiglin.getFetchVersion().checkForUpdates()) {
-            sender.sendMessage(GoldPiglin.getFetchVersion().getUpdateMessage());
-        } else {
-            sender.sendMessage(GoldPiglin.getFetchVersion().getNoUpdateMessage());
-        }
+        if (GoldPiglin.getFetchVersion().checkForUpdates()) sender.sendMessage(GoldPiglin.getFetchVersion().getUpdateMessage());
+        else sender.sendMessage(GoldPiglin.getFetchVersion().getNoUpdateMessage());
     }
 
     @Override
     public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) {
         List<String> list = Util.newArrayList();
-        if (args.length == 0) {
-            if (sender.hasPermission("goldpiglin.command.reload")) {
-                list.add("reload");
-            }
-            if (sender.hasPermission("goldpiglin.command.version")) {
-                list.add("version");
-            }
+        if (args.length == 1) {
+            if (sender.hasPermission("goldpiglin.command.reload")) list.add("reload");
+            if (sender.hasPermission("goldpiglin.command.version")) list.add("version");
         }
         return list;
     }
